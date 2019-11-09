64036db198fa83e47003ab5541049d4c0a4984b3
clockwerkz <clockwerkz@gmail.com>
2019-11-08 21:36:39 -0800

    adds loops challenge code

---
+++ Assets/Scripts/LoopsTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoopsTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	//	for (var i = -5; i<=5; i++)
+    //    {
+    //        print(i);
+    //    }
+
+     //   for (var i = 10; i<=50; i++)
+     //   {
+     //       if ( i % 2 == 0)
+     //       {
+     //           print(i);
+     //       }
+     //   }
+
+        for (var i=1; i<=100; i++)
+        {
+            if (i % 15 == 0)
+            {
+                print(i);
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
